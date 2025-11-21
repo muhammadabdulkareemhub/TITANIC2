d0808b39df81f4bbc7c47aec44291911e3a1ab1d
unknown <nuxabdol@gmail.com>
2025-11-21 19:20:07 +0100

    update

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import pickle
-import  numpy as np
+import numpy as np
 
-file=open("titanic_model.pkl","rb")
-model=pickle.load(file)
+# Load model
+with open("titanic_model.pkl", "rb") as file:
+    model = pickle.load(file)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,6 +18,7 @@ def about():
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    # Collect form values
     Pclass = request.form['Pclass']
     Sex = request.form['Sex']
     Age = float(request.form['Age'])
@@ -24,40 +27,36 @@ def submit():
     Fare = float(request.form['Fare'])
     Embarked = float(request.form['Embarked'])
     Age_Group = float(request.form['Age Group'])
-    
 
+    # Encode Pclass
     if Pclass == "1":
-        Pclass = 0
+        Pclass_encoded = 0
     elif Pclass == "2":
-        Pclass = 1
-    elif  Pclass == "3":
-        Pclass = 2
+        Pclass_encoded = 1
+    else:
+        Pclass_encoded = 2
 
+    # Encode Sex
+    Sex_encoded = 1 if Sex == "male" else 0
 
-    if Sex == 'male': 
-        Sex = 1
-    else:   
-        Sex = 0
+    # Build feature array
+    features = np.array([[Pclass_encoded, Sex_encoded, Age, SibSp, Parch, Fare, Embarked, Age_Group]])
 
-
-
-    features = np.array([[Pclass, Sex, Age, SibSp, Parch, Fare, Embarked, Age_Group]])
+    # Model prediction
     prediction = model.predict(features)
 
-    
-
-
-    return render_template("result.html", Pclass=Pclass,
-                            Sex=Sex, 
-                            Age=Age,  
-                            Parch=Parch, 
-                            SibSp=SibSp,
-                            Fare=Fare,
-                            Embarked=Embarked,
-                            Age_Group=Age_Group,
-                            prediction=prediction[0])
-
+    return render_template(
+        "result.html",
+        Pclass=Pclass,          # original value for display
+        Sex=Sex,                # original sex (male/female)
+        Age=Age,
+        SibSp=SibSp,
+        Parch=Parch,
+        Fare=Fare,
+        Embarked=Embarked,
+        Age_Group=Age_Group,
+        prediction=prediction[0]
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
-
